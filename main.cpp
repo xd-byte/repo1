d0eb56992c951775b6d6b985f1448c77ea0c9a2f
成宇 宁 <ningchengyu0906@163.com>
2023-01-07 10:42:23 +0800

    test

--- main.cpp
+++ main.cpp
@@ -2,17 +2,17 @@
 
 int add(int lhs, int rhs)
 {
-    return  lhs+rhs;
+    return  lhs*rhs;
 }
 
 int sub(int lhs, int rhs)
 {
-    return  lhs-rhs;
+    return  lhs/rhs;
 }
 
 int multiply(int lhs, int rhs)
 {
-    return  lhs*rhs;
+    return  lhs+rhs;
 }
 
 int main(int* argc, char** argv)
